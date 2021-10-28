f47b754e27a5b6152625a24544da0795bc773a51
Azeem M <87535744+azeem-mustafa@users.noreply.github.com>
2021-10-28 15:48:13 -0400

    completed stylings for page

--- hatchways-assessment/src/App.js
+++ hatchways-assessment/src/App.js
@@ -6,11 +6,11 @@ class App extends Component {
   state = {
     userData: [],
   };
+  
 
   componentDidMount() {
     axios.get('https://www.hatchways.io/api/assessment/students')
       .then(res => {
-        console.log(res.data.students)
         this.setState({ userData: res.data.students })
       })
   }
@@ -18,24 +18,35 @@ class App extends Component {
     const { userData } = this.state;
 
     return (
-      <div className='data'>
-        <ul>
-          {userData.map(item => (
-            <li key={item.name}>
-              <img className='data__pic' src={item.pic} alt='profile pic' />
-              <h1 className='data__name'>{item.firstName}</h1>
-              <h2 className='data__email'>Email: {item.email}</h2>
-              <h2 className='data__company'>Company: {item.company}</h2>
-              <h2 className='data__skill'>Skill: {item.skill}</h2>
-              <h2 className='data__grades'>Average: {item.grades.reduce((a,b) => +a + +b)/item.grades.length}%</h2>
-            </li>
 
-          ))}
-        </ul>
+      <div className='data'>
+        <div className='data__wrapper'>
+          <ul className='data__ul'>
+            {userData.map(item => (
+              <li className='data__li' key={item.name}>
+                
+                <div className='data__li-wrapper-pic'>
+                  <img className='data__pic' src={item.pic} alt='profile pic' />
+                </div>
+
+                <div className='data__li-wrapper-information-main'>
+                  <h1 className='data__name'>{item.firstName.toUpperCase()} {item.lastName.toUpperCase()}</h1>
+
+                  <div className='data__li-wrapper-information-sub'>
+                    <p className='data__detail data__email'>Email: {item.email}</p>
+                    <p className='data__detail data__company'>Company: {item.company}</p>
+                    <p className='data__detail data__skill'>Skill: {item.skill}</p>
+                    <p className='data__detail data__grades'>Average: {item.grades.reduce((a, b) => +a + +b) / item.grades.length}%</p>
+                  </div>
+
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     )
   }
 }
-
 
 export default App;
